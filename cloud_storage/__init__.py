c89cd649b7fbf7f322df25861d7b349bf0f31581
jigar-greycube <jigar@greycube.in>
2026-07-13 17:54:16 +0530

    minor:version upgrade to 0.0.4

--- cloud_storage/__init__.py
+++ cloud_storage/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, AgriTheory and contributors
 # For license information, please see license.txt
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
 import frappe.desk.form.load
